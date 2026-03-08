6ac5dbeb4877fc61255f0193cc7d69b9f74dac0d
roman <168015073+Roman-Akimov@users.noreply.github.com>
2026-03-08 15:53:42 +0400

    #Обновленный store для локализации и поиска

--- test-task/src/store/useAppStore.ts
+++ test-task/src/store/useAppStore.ts
@@ -1,35 +1,51 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
+import type { Product } from '../types/product';
+export type { Product } from '../types/product';
 
 interface AppState {
   theme: 'light' | 'dark';
   lang: 'ru' | 'en';
+  searchQuery: string;
+  selectedProduct: Product | null;
   toggleTheme: () => void;
   toggleLang: (lang: 'ru') => void;
+  setSearchQuery: (query: string) => void;
+  setProduct: (product: Product | null) => void;
 }
 
 export const useAppStore = create<AppState>()(
   persist(
     (set) => ({
-      // Начальные значения
       theme: 'light',
       lang: 'ru',
+      searchQuery: '',
+      selectedProduct: null,
 
-      // Переключение темы
       toggleTheme: () =>
         set((state) => ({
           theme: state.theme === 'light' ? 'dark' : 'light',
         })),
 
-      // Переключение языка
       toggleLang: () =>
         set((state) => ({
           lang: state.lang === 'ru' ? 'en' : 'ru',
         })),
+
+      setSearchQuery: (query) => set({ searchQuery: query }),
+
+      setProduct: (product) =>
+        set({
+          selectedProduct: product,
+        }),
     }),
     {
       name: 'app-storage',
       storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({
+        theme: state.theme,
+        lang: state.lang,
+      }),
     }
   )
 );
